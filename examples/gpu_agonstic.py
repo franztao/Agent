7f6a74e1e1bbc70ad7cf6657cbfd0a51b47f2803
franztao <franztaoheng@gmail.com>
2025-04-22 16:07:04 +0800

    1.跑任务存日志 2.获取状态信息存日志 3.读取日志解析状态，判断是否终止

--- examples/gpu_agonstic.py
+++ examples/gpu_agonstic.py
@@ -1,3 +1,7 @@
+import sys
+
+sys.path.append('/home/hengtao/code/diagnostic/metax_gpu_diagnostic_suite')
+
 from agent.tools.libs.shell_tool import shell_tool
 
 
@@ -17,19 +21,25 @@ def mx_smi_topo__show_mxlk():
     r = shell_tool(tool_input)
     r_agonstic = agonstic_mx_smi_topo__show_mxlk(r)
     r_reason = reason_mx_smi_topo__show_mxlk(r)
-    
+
     pass
+
 
 def mxvs_p2p__unidirection():
     tool_input = "/opt/maca/bin/mxvs p2p --unidirection --dst-devices 0 --src-devices 0"
+    tool_input = "/opt/maca/bin/mxvs p2p --unidirection  --src-devices 0,1,2,3,4,5,6,7 --dst-devices 0,1,2,3,4,5,6,7"
     r = shell_tool(tool_input)
 
+
 def mxvs_p2p():
-    tool_input = "/opt/maca/bin/mxvs  --unidirection --dst-devices 0 --src-devices 0"
+    tool_input = "/opt/maca/bin/mxvs  p2p --dst-devices 0 --src-devices 0"
+    tool_input = "/opt/maca/bin/mxvs  p2p  --src-devices 0,1,2,3,4,5,6,7 --dst-devices 0,1,2,3,4,5,6,7"
     r = shell_tool(tool_input)
+
 
 def mxvs_ops():
     tool_input = "/opt/maca/bin/mxvs  ops"
+    tool_input = "/opt/maca/bin//mxvs p2p --src-devices 0,1,2,3,4,5,6,7 --dst-devices 0,1,2,3,4,5,6,7"
     r = shell_tool(tool_input)
 
 
